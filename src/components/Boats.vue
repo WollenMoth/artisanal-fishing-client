ae6cb61874c3cf844d9760dab61ea36ef8e22f52
Raul Antonio Chavez Aguirre <54995852+xXChAvE2Xx@users.noreply.github.com>
2022-05-15 19:27:57 -0500

    Se creo el componente boat

---
+++ src/components/Boats.vue
@@ -0,0 +1,162 @@
+<template>
+  <h4>Capitán</h4>
+  <div
+    v-if="currentBoat && currentCompanie && currentCaptain"
+    class="edit-form"
+  >
+    <form>
+      <div class="form-group">
+        <label for="title">Nombre de la embarcación</label>
+        <input
+          type="text"
+          class="form-control"
+          id="title"
+          v-model="currentBoat.name"
+        />
+      </div>
+      <div class="form-group">
+        <label for="title">Nombre del capitan</label>
+        <input
+          type="text"
+          disabled
+          class="form-control"
+          id="title"
+          v-model="currentCaptain.name"
+        />
+      </div>
+      <div class="form-group">
+        <label for="title">Nombre de la compañia</label>
+        <input
+          type="text"
+          disabled
+          class="form-control"
+          id="title"
+          v-model="currentCompanie.name"
+        />
+      </div>
+      <div class="form-group">
+        <label for="title">Locación:</label>
+        <input
+          type="text"
+          disabled
+          class="form-control"
+          id="title"
+          v-model="currentBoat.fishingLocation"
+        />
+      </div>
+      <div class="form-group">
+        <label for="title">Lugar de zarpe:</label>
+        <input
+          type="text"
+          class="form-control"
+          id="title"
+          v-model="currentBoat.zarpe"
+        />
+      </div>
+      <div class="form-group">
+        <label for="title">¿Qué especies captura?</label>
+        <input
+          type="text"
+          class="form-control"
+          id="title"
+          v-model="currentBoat.capture"
+        />
+      </div>
+    </form>
+    <button class="btn btn-danger mr-2" @click="deleteBoat">
+      Eliminar embarcación
+    </button>
+    <button type="submit" class="btn btn-info mr-2" @click="updateBoat">
+      Actualizar
+    </button>
+    <p>{{ message }}</p>
+    <div v-if="error" class="alert alert-danger" role="alert">
+      {{ error }}
+    </div>
+  </div>
+  <div v-else>
+    <br />
+    <p>Selecciona una embarcación.</p>
+  </div>
+</template>
+<script>
+import BoatService from '../services/BoatService.js';
+import CaptainService from '../services/CaptainService.js';
+import CompanieService from '../services/CompanieService.js';
+
+export default {
+  name: 'boats',
+  data() {
+    return {
+      currentBoat: null,
+      currentCaptain: null,
+      currentCompanie: null,
+      message: '',
+      error: '',
+    };
+  },
+  methods: {
+    getBoat(id) {
+      BoatService.get(id)
+        .then((response) => {
+          this.currentBoat = response.data;
+        })
+        .catch((e) => {
+          this.error = e.response.data.message;
+        });
+    },
+    deleteBoat() {
+      BoatService.delete(this.currentBoat.id)
+        .then((response) => {
+          console.log(response.data);
+          this.$router.push({ name: 'boats' });
+        })
+        .catch((e) => {
+          this.error = e.response.data.message;
+        });
+    },
+    updateBoat() {
+      BoatService.update(this.currentBoat.id, this.currentBoat)
+        .then((response) => {
+          console.log(response.data);
+          this.message = 'Se actualizó correctamente';
+        })
+        .catch((e) => {
+          this.error = e.response.data.message;
+        });
+    },
+    getCaptain(id) {
+      CaptainService.get(id)
+        .then((response) => {
+          this.currentCaptain = response.data;
+        })
+        .catch((e) => {
+          this.error = e.response.data.message;
+        });
+    },
+    getCompanie(id) {
+      CompanieService.get(id)
+        .then((response) => {
+          this.currentCompanie = response.data;
+        })
+        .catch((e) => {
+          this.error = e.response.data.message;
+        });
+    },
+  },
+  mounted() {
+    this.message = '';
+    this.error = '';
+    this.getBoat(this.$route.params.id);
+    this.getCaptain(this.$route.params.id);
+    this.getCompanie(this.$route.params.id);
+  },
+};
+</script>
+
+<style>
+.edit-form {
+  max-width: 300px;
+  margin: auto;
+}
+</style>
---
+++ src/components/BoatsList.vue
@@ -0,0 +1,110 @@
+<template>
+  <div class="list row">
+    <div class="col-md-6">
+      <h4>Botes Registrados</h4>
+      <ul class="list-group">
+        <li
+          class="list-group-item"
+          :class="{ active: index == currentIndex }"
+          v-for="(boat, index) in boats"
+          :key="index"
+          @click="setActiveBoat(boat, index)"
+        >
+          {{ boat.name }}
+        </li>
+      </ul>
+    </div>
+    <div class="col-md-6">
+      <div v-if="currentBoat && currentCaptain && currentCompanie">
+        <h4>Embarcación</h4>
+        <div>
+          <br />
+          <label><strong>Nombre del capitán:</strong></label>
+          {{ currentCaptain.name }}<br />
+          <label><strong>Nombre de la embarcación:</strong></label>
+          {{ currentBoat.name }}<br />
+          <label><strong>Nombre de la compañia:</strong></label>
+          {{ currentCompanie.name }}<br />
+          <label><strong>Fecha de pesca:</strong></label>
+          {{ currentBoat.fishingDate }}<br />
+          <label><strong>Locación:</strong></label>
+          {{ currentBoat.fishingLocation }}<br />
+          <label><strong>Lugar de zarpe:</strong></label>
+          {{ currentBoat.zarpe }}<br />
+          <label><strong>Captura:</strong></label>
+          {{ currentBoat.capture }}<br />
+        </div>
+        <router-link :to="'/boats/' + currentBoat.id" class="btn btn-info">
+          Editar</router-link
+        >
+      </div>
+      <div v-else>
+        <br />
+        <p>Selecciona un bote.</p>
+        <router-link to="/add-boat" class="btn btn-info"
+          >Agregar bote</router-link
+        >
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import BoatService from '../services/BoatService.js';
+import CaptainService from '../services/CaptainService.js';
+import CompanieService from '../services/CompanieService.js';
+
+export default {
+  name: 'boats-List',
+  data() {
+    return {
+      tutorials: [],
+      boats: [],
+      currentTutorial: null,
+      currentBoat: null,
+      currentCaptain: null,
+      currentCompanie: null,
+      currentIndex: -1,
+      title: '',
+      BoatId: '',
+    };
+  },
+  methods: {
+    getAllBoats() {
+      BoatService.getAll()
+        .then((response) => {
+          this.boats = response.data;
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    },
+    getCaptain(id) {
+      CaptainService.get(id)
+        .then((response) => {
+          this.currentCaptain = response.data;
+        })
+        .catch((e) => {
+          this.error = e.response.data.message;
+        });
+    },
+    getCompanie(id) {
+      CompanieService.get(id)
+        .then((response) => {
+          this.currentCompanie = response.data;
+        })
+        .catch((e) => {
+          this.error = e.response.data.message;
+        });
+    },
+    setActiveBoat(boat, index) {
+      this.currentBoat = boat;
+      this.currentIndex = boat ? index : -1;
+      this.getCaptain(boat.id);
+      this.getCompanie(boat.id);
+    },
+  },
+  mounted() {
+    this.getAllBoats();
+  },
+};
+</script>
